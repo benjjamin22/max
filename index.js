f64f5bda22f8601efc2f615df4c21b97aa7f4e22
benjjamin22 <122483173+benjjamin22@users.noreply.github.com>
2024-03-20 13:26:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('trust proxy', 1)
 app.use(cors())
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public'">÷÷£)));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/api', require('./routes'))
